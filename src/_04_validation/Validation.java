59dc68f553250f3591b57994002d1a3fea0fb0ad
i-lab user <i-labuser@SDPL88534-iMacProL.local>
2020-02-04 17:57:51 -0800

    working in validation

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -17,7 +17,11 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
+if(randomNumber==0) {
+System.out.println("you are smart");	
+{
 
+}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
