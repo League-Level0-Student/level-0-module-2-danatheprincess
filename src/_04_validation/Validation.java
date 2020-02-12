ce800143255e05e2e5b51a9b931d7f94070b487d
i-lab user <i-labuser@SDPL88534-iMacProL.local>
2020-02-11 17:54:12 -0800

    doing magic 8 ball

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -9,21 +9,33 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
+		for(int d=1;d<10;d++) {
 		Random randomMaker = new Random();
 		
 		int randomNumber = randomMaker.nextInt(5);
 		
-		System.out.println(randomNumber);
+		
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-if(randomNumber==0) {
-System.out.println("you are smart");	
-{
-
+		if(randomNumber==0) {
+			
+			System.out.println("you are smart");	
+		}
+		if(randomNumber==1) {
+		System.out.println("you are hard working");
+		}
+		if(randomNumber==2) {
+		System.out.println("you are inspiring");
 }
-		// 2. Repeat all the code above 10 times
-		
-		// 3. Find someone to test out your program. They will like it :)
+		if(randomNumber==3) {
+			System.out.println("you have a nice name");
+		}
+		if(randomNumber==4) {		
+		System.out.println("I like your shoes");
+		}
+			// 2. Repeat all the code above 10 times
+			
+			// 3. Find someone to test out your program. They will like it :)
+		}
 	}
 }
